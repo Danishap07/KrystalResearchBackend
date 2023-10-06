e55eb447532575f284e251e7c9ae7de94ea9db88
danishap07 <danishp.1999@gmail.com>
2023-10-06 17:36:05 +0530

    third_commit

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
     'http://localhost:8800',
     'localhost:8800',
+    'https://krystal-research-backend.onrender.com',
+    'krystal-research-backend.onrender.com'
 ]
 
 module.exports = allowedOrigins;
